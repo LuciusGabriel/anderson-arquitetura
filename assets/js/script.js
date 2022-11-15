0d974853e2614b4b77a5df9cb54d3a6ae7d6517b
LuciusGabriel <lucius_gabriel@hotmail.com>
2022-11-14 22:08:16 -0300

    Inicio Projeto

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+/*function imagens(){
+
+    var img = document.querySelector('#slide')
+    var image = document.createElement('img')
+
+    image.setAttribute('src', 'img/img-1.jpg')
+    image.setAttribute('id', 'imagem')
+
+    img.appendChild(image)
+
+}*/
