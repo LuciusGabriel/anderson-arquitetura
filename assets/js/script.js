de6100c92d41c5d0e84714706e89c3e142d468a3
LuciusGabriel <lucius_gabriel@hotmail.com>
2023-03-16 23:18:39 -0300

    Inserir redes sociais

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,25 @@ function imagens(){
     img.style.backgroundImage = "url('./assets/img/img-2.jpg')"
 }
 setInterval(imagens, interval)
+
+
+
+const instagram = document.querySelector('.insta')
+instagram.addEventListener('mouseover', ()=>{
+    instagram.src = 'assets/img/icons/instagram-color.svg'
+    console.log('Passou')
+})
+instagram.addEventListener('mouseout', ()=>{
+    instagram.src = 'assets/img/icons/instagram-pb.svg'
+})
+
+
+const linkedin = document.querySelector('.linked')
+linkedin.addEventListener('mouseover', ()=>{
+    linkedin.src = 'assets/img/icons/linkedin-color.svg'
+    console.log('Passou')
+})
+linkedin.addEventListener('mouseout', ()=>{
+    linkedin.src = 'assets/img/icons/linkedin-pb.svg'
+})
+
